4704097225d9b97de928ea46567f10ac51201e42
Pamela Feijó <64004289+PamelaFeijo@users.noreply.github.com>
2021-02-06 15:03:08 +0100

    Initial commit

---
+++ rock-paper-or-scissors/index.js
@@ -0,0 +1,62 @@
+const getUserChoice = (userInput) => {
+  userInput = userInput.toLowerCase();
+  if (userInput === "rock") {
+    return userInput;
+  } else if (userInput === "paper") {
+    return userInput;
+  } else if (userInput === "scissors") {
+    return userInput;
+  } else {
+    return "Error";
+  }
+};
+
+const getComputerChoice = () => {
+  const randomNumber = Math.floor(Math.random() * 3);
+  if (randomNumber === 0) {
+    return "rock";
+  } else if (randomNumber === 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+};
+
+const determineWinner = (userChoice, computerChoice) => {
+  if (userChoice === computerChoice) {
+    return "The game was a tie.";
+  }
+
+  if (userChoice === "rock") {
+    if (computerChoice === "paper") {
+      return "The computer won!";
+    } else {
+      return "You won!";
+    }
+  }
+  if (userChoice === "paper") {
+    if (computerChoice === "scissors") {
+      return "The computer won!";
+    } else {
+      return "You won!";
+    }
+  }
+  if (userChoice === "scissors") {
+    if (computerChoice === "rock") {
+      return "The computer won!";
+    } else {
+      return "You won!";
+    }
+  }
+};
+
+const playGame = () => {
+  const userChoice = getUserChoice("paper");
+  const computerChoice = getComputerChoice();
+  console.log(`You threw: ${userChoice}`);
+  console.log(`The computer threw: ${computerChoice}`);
+
+  console.log(determineWinner(userChoice, computerChoice));
+};
+
+playGame();
